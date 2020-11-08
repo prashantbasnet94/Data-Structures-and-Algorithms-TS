6e288dc24993ba90b1752dc9eac281ae9b0d4a02
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2020-11-07 22:16:06 -0600

    push, pop, delete added

--- mine/Array/Array.js
+++ mine/Array/Array.js
@@ -11,13 +11,34 @@ var Array_Made = /** @class */ (function () {
             this.length++;
         }
     };
+    Array_Made.prototype.pop = function () {
+        delete this.data[this.length - 1];
+        this.length--;
+    };
     Array_Made.prototype.printArray = function () {
         console.log(this.data);
         console.log(this.length);
+    };
+    Array_Made.prototype["delete"] = function (index) {
+        this.shiftItems(index);
+    };
+    Array_Made.prototype.shiftItems = function (index) {
+        //run loop to shift and replace the item from the index
+        for (var startFromGivenIndex = index; startFromGivenIndex < this.length - 1; startFromGivenIndex++) {
+            this.data[startFromGivenIndex] = this.data[startFromGivenIndex + 1];
+        }
+        // Last item is moved a step ahead but also exist at the same place @ the end
+        // we need to delete the original last item
+        //delete this.data[this.length-1];
+        this.length--;
     };
     return Array_Made;
 }());
 var object_of_array = new Array_Made();
 object_of_array.push(5);
 object_of_array.push(15);
+object_of_array.push(25);
+object_of_array.push(35);
+object_of_array.printArray();
+object_of_array["delete"](2);
 object_of_array.printArray();
--- mine/Array/Array.ts
+++ mine/Array/Array.ts
@@ -22,9 +22,37 @@ class Array_Made {
         }
     }
 
+    public pop(){
+        delete this.data[this.length-1]
+        this.length--;
+    }
+
     public printArray(){
         console.log(this.data)
         console.log(this.length)
+    }
+
+    public delete(index:number){
+        this.shiftItems(index)
+    }
+
+    public shiftItems(index:number){
+
+        //run loop to shift and replace the item from the index
+        for
+        ( let startFromGivenIndex =index;startFromGivenIndex<this.length-1;startFromGivenIndex++)
+        {
+            this.data[startFromGivenIndex]= this.data[startFromGivenIndex+1]
+        }
+
+        // Last item is moved a step ahead but also exist at the same place @ the end
+        // we need to delete the original last item
+        // { '0': 5, '1': 15, '2': 25, '3': 35 }
+        // { '0': 5, '1': 15, '2': 35, '3': 35 }
+
+        delete this.data[this.length-1];
+        this.length--;
+
     }
 
 }
@@ -32,4 +60,9 @@ class Array_Made {
 let object_of_array = new Array_Made();
 object_of_array.push(5)
 object_of_array.push(15)
+object_of_array.push(25)
+object_of_array.push(35)
+object_of_array.printArray()
+
+object_of_array.delete(2)
 object_of_array.printArray()
